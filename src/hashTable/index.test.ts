57d088643cd1b0cf9256a0fc016a0b4a543db92f
hope_lkn <nadezhda.lkn@yahoo.com>
2021-03-22 19:22:48 +0300

    Make hash table for all types of keys

--- src/hashTable/index.test.ts
+++ src/hashTable/index.test.ts
@@ -10,10 +10,10 @@ test('HASH FUNCTION: returns unique indices for anagrams', () => {
     }
 });
 
-test('HASH FUNCTION: returns null for objects, arrays and functions', () => {
-    expect(hash([2,4,6,'p'])).toBe(null);
-    expect(hash({j: 1, k: 'k'})).toBe(null);
-    expect(hash(() => {})).toBe(null);
+test('KEY TO STRING FUNCTION: returns string for objects, arrays and functions', () => {
+    expect(keyToString([2,4,6,'p'])).toBe('2,4,6,p');
+    expect(keyToString({j: 1, k: 'k'})).toBe('j_1');
+    expect(keyToString(() => {})).toBe('() => {}');
 });
 
 test('KEY TO STRING FUNCTION: returns strings for various types of keys', () => {
@@ -23,7 +23,11 @@ test('KEY TO STRING FUNCTION: returns strings for various types of keys', () => 
 });
 
 test('HASH TABLE: adds and finds values', () => {
-    const hashTable = new HashTable(7);
+    const hashTable = new HashTable(10);
+
+    const obj = {hey: 'juice'};
+    const func = () => {console.log('hello')};
+    const arr = [false, 2,4]
 
     hashTable.addValue('juice', 150);
     hashTable.addValue('milk', 30);
@@ -32,9 +36,12 @@ test('HASH TABLE: adds and finds values', () => {
     hashTable.addValue(false, 300);
     hashTable.addValue(405, 350);
     hashTable.addValue(9007199254740991n, 'hey there');
+    hashTable.addValue(obj, 2224);
+    hashTable.addValue(func, 3050);
+    hashTable.addValue(arr, 11111);
 
-    const keys = ['juice', 'milk', 'oranges', 'butter', false, 405, 9007199254740991n];
-    const values = [150, 30, 200, 100, 300, 350, 'hey there'];
+    const keys = ['juice', 'milk', 'oranges', 'butter', false, 405, 9007199254740991n, obj, func, arr];
+    const values = [150, 30, 200, 100, 300, 350, 'hey there', 2224, 3050, 11111];
 
     keys.forEach((key, index) => {
         expect(hashTable.findValue(key)).not.toBeUndefined();
@@ -43,18 +50,26 @@ test('HASH TABLE: adds and finds values', () => {
 });
 
 test('HASH TABLE: updates values with identical keys', () => {
-    const hashTable = new HashTable(3);
+    const hashTable = new HashTable(6);
+
+    const obj = {hey: 'juice'};
+    const func = () => {console.log('hello')};
+    const arr = [false, 2,4]
 
     hashTable.addValue('butter', 'cheap');
-    hashTable.addValue(405, 111);
     hashTable.addValue(9007199254740991n, 'hey there');
+    hashTable.addValue(obj, 111);
+    hashTable.addValue(func, 30);
+    hashTable.addValue(arr, 30);
 
     hashTable.addValue('butter', 'expensive');
-    hashTable.addValue(405, 222);
     hashTable.addValue(9007199254740991n, 'bye there');
+    hashTable.addValue(obj, 222);
+    hashTable.addValue(func, 300);
+    hashTable.addValue(arr, 305);
 
-    const keys = ['butter', 405, 9007199254740991n];
-    const values = ['expensive', 222, 'bye there'];
+    const keys = ['butter', obj, 9007199254740991n, func, arr];
+    const values = ['expensive', 222, 'bye there', 300, 305];
 
     keys.forEach((key, index) => {
         expect(hashTable.findValue(key)).not.toBeUndefined();
@@ -65,22 +80,35 @@ test('HASH TABLE: updates values with identical keys', () => {
 test('HASH TABLE: removes values', () => {
     const hashTable = new HashTable(10);
 
+    const obj = {hey: 'juice'};
+    const func = () => {console.log('hello')};
+    const arr = [false, 2,4]
+
     hashTable.addValue('juice', 150);
     hashTable.addValue('milk', 30);
     hashTable.addValue(false, 30);
     hashTable.addValue(405, 30);
+    hashTable.addValue(obj, 30);
+    hashTable.addValue(func, 30);
+    hashTable.addValue(arr, 30);
     hashTable.removeValue('juice');
     hashTable.removeValue(false);
     hashTable.removeValue(405);
+    hashTable.removeValue(arr);
+    hashTable.removeValue(obj);
+    hashTable.removeValue(func);
 
 
     expect(hashTable.findValue('milk')).not.toBeUndefined();
     expect(hashTable.findValue('juice')).toBeUndefined();
     expect(hashTable.findValue(405)).toBeUndefined();
     expect(hashTable.findValue(false)).toBeUndefined();
+    expect(hashTable.findValue(arr)).toBeUndefined();
+    expect(hashTable.findValue(obj)).toBeUndefined();
+    expect(hashTable.findValue(func)).toBeUndefined();
 });
 
-test('HASH TABLE: does not add values with unhashable keys', () => {
+test('HASH TABLE: adds values with not primitive keys', () => {
     const hashTable = new HashTable(10);
 
     const obj = {hey: 'juice'};
@@ -91,8 +119,11 @@ test('HASH TABLE: does not add values with unhashable keys', () => {
     hashTable.addValue(arr, 30);
     hashTable.addValue(func, 30);
 
-    expect(hashTable.findValue(obj)).toBeUndefined();
-    expect(hashTable.findValue(arr)).toBeUndefined();
-    expect(hashTable.findValue(func)).toBeUndefined();
+    expect(hashTable.findValue(obj)).not.toBeUndefined();
+    expect(hashTable.findValue(obj)).toBe(150);
+    expect(hashTable.findValue(arr)).not.toBeUndefined();
+    expect(hashTable.findValue(arr)).toBe(30);
+    expect(hashTable.findValue(func)).not.toBeUndefined();
+    expect(hashTable.findValue(func)).toBe(30);
 
 });
--- src/hashTable/index.ts
+++ src/hashTable/index.ts
@@ -1,18 +1,26 @@
 const PRIME_BASE_NUMBER = 307;
 
+export const handleObjectToString = (object: object | Array<any>): string => {
+    if (Array.isArray(object)) {
+        return object.toString();
+    }
+
+    const firstKey = Object.keys(object)[0];
+
+    return firstKey.toString() + '_' + object[firstKey].toString();
+}
+
 export const keyToString = (key: any): string => {
     if (typeof key === 'string') {
         return key;
+    }
+    if (typeof key === 'object') {
+        return handleObjectToString(key);
     }
     return key.toString();
 }
 
 export const hash = (key: any): number | null => {
-    if (typeof key === 'object' || typeof key === 'function') {
-        console.log('Provided key is not hashable');
-        return null;
-    }
-
     let hash = 0;
     let keyString = keyToString(key);
     for (let i = 0; i < keyString.length; i++) {
@@ -33,12 +41,17 @@ export class HashTable<KeyT, ValueT> {
     addValue = function(key: KeyT, value: ValueT): void {
         const hashedKey = hash(key);
         let index = hashedKey !== null ?  hashedKey % this.tableSize : hashedKey;
+        const isNotPrimitiveKey = typeof key === 'object' || typeof key === 'function';
 
         if (index !== null) {
             // if we have this slot occupied
             while (this.storage[index] !== undefined) {
                 // if the key is identical - we stop searching for an empty slot and update the value
-                if (this.storage[index][0] === key) {
+                if (
+                    (!isNotPrimitiveKey && this.storage[index][0] === key)
+                    || (isNotPrimitiveKey && typeof this.storage[index][0] === 'bigint')
+                    || (isNotPrimitiveKey && JSON.stringify(this.storage[index][0]) === JSON.stringify(key))
+                ) {
                     break;
                 }
                 index += 1 % this.tableSize;
@@ -50,28 +63,39 @@ export class HashTable<KeyT, ValueT> {
     removeValue = function(key: KeyT): void {
         const hashedKey = hash(key);
         let index = hashedKey !== null ?  hashedKey % this.tableSize : hashedKey;
-        if (hashedKey!== null && !!this.storage[index] && this.storage[index][0] === key) {
+        const isNotPrimitiveKey = typeof key === 'object' || typeof key === 'function';
+
+        if (hashedKey!== null
+            && !!this.storage[index]
+            && ((!isNotPrimitiveKey && this.storage[index][0] === key)
+                || (isNotPrimitiveKey && typeof this.storage[index][0] === 'bigint')
+                || (isNotPrimitiveKey && JSON.stringify(this.storage[index][0]) === JSON.stringify(key)))
+        ) {
             delete this.storage[index]
         } else {
-            console.error(`Element with key "${key}" not found`)
+            console.log(`Element with key "${key}" not found`)
         }
     }
 
     findValue = function(key: KeyT): ValueT | undefined {
         const hashedKey = hash(key);
+        const isNotPrimitiveKey = typeof key === 'object' || typeof key === 'function';
         if (hashedKey === null) {
-            console.log('Provided key is not hashable');
             return undefined;
         }
 
         let index = hashedKey % this.tableSize;
         // if we have this slot occupied
         if (!!this.storage[index] ) {
+
             // if the slot is occupied with another key - it's a collision
-            while (this.storage[index][0] !== key) {
+            while ((!isNotPrimitiveKey && this.storage[index][0] !== key)
+                || (isNotPrimitiveKey && typeof this.storage[index][0] === 'bigint')
+                || (isNotPrimitiveKey && JSON.stringify(this.storage[index][0]) !== JSON.stringify(key))
+                ) {
                 index += 1 % this.tableSize;
-                break;
             }
+
             return this.storage[index][1];
         } else {
             console.log(`Element with key "${key}" is undefined`);
